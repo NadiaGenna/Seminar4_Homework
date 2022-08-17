594752313a54a5df48f89289f275ab67faf25bcb
«Nadezhda <nadezhdagenna@mail.ru>
2022-08-17 14:05:29 +0300

    Task29 is ready

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -3,8 +3,8 @@
 
 int [] NewArray ()
 {
-    int[] array = new int [8];
-    for (int i = 0; i <=7; i++)
+    int[] array = new int[8];
+    for (int i = 0; i <= 7; i++)
     {
         array[i] = new Random().Next(1, 100);
     }
